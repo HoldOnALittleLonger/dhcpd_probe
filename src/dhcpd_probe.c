6f1f323feaf74ce84a9b1ef0b6304dd0ea01b7df
Zephyr <emailunspecified@false.com>
2025-03-03 00:16:46 +0800

    Changes :         rename "interface_address.h" to "if_query.h"         rename "interface_address.c" to "if_query.c"         rebuild netdevice interface information query features         introduced structure ip_packet to describe IP packet         introduced structure udp_segment to describe UDP segment         make use of link-level socket to receive raw data from ether network

--- src/dhcpd_probe.c
+++ src/dhcpd_probe.c
@@ -1,26 +1,26 @@
 #include "dhcp_msg.h"
 #include "dhcp_routines.h"
-#include "interface_address.h"
+#include "if_query.h"
 
 #include <stdint.h>
 #include <string.h>
+#include <stdio.h>
 
 #include <sys/socket.h>
 #include <arpa/inet.h>
-
-#include <stdio.h>
-
-#include <errno.h>
+#include <net/ethernet.h>
+#include <linux/if_packet.h>
 
 #define PRINTF_TO_STDERR(str) ({fprintf(stderr, "%s\n", (str));})
 
 int dhcpd_probe(const char *interface)
 {
-        uint8_t mac_address[HWADDR_802_3_LEN] = {0};
-        if (get_hwaddr(interface, mac_address, HWADDR_802_3_LEN) < 0) {
+        uint8_t mac_address[ETHER_ADDR_LEN] = {0};
+        if (start_ifquery(interface) < 0) {
                 PRINTF_TO_STDERR(dhcpd_probe_error(DHCPFUNC_ESYSCALL));
                 return -1;
         }
+        if_hw_addr(mac_address, ETHER_ADDR_LEN);
 
         int ret = 0;
         int socketfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
@@ -29,31 +29,17 @@ int dhcpd_probe(const char *interface)
                 return -1;
         }
 
-        if (setsockopt(socketfd, SOL_SOCKET,
-                       SO_BINDTODEVICE, interface, strlen(interface))
-            < 0) {
-                PRINTF_TO_STDERR(dhcpd_probe_error(DHCPFUNC_ESOCKFD));
-                ret = -1;
-                goto quit_nomalloc;
-        }
-
+        /* enable broadcast */
         ret = 1;
         if (setsockopt(socketfd, SOL_SOCKET, SO_BROADCAST, &ret, sizeof(ret)) < 0) {
-                PRINTF_TO_STDERR(dhcpd_probe_error(DHCPFUNC_ESOCKFD));
+                PRINTF_TO_STDERR(dhcpd_probe_error(DHCPFUNC_EBROADCAST));
                 ret = -1;
                 goto quit_nomalloc;
         }
 
-        struct sockaddr_in local_addr = {
-                .sin_family = AF_INET,
-                .sin_port = htons(DHCPC_RECV_PORT),
-                .sin_addr.s_addr = INADDR_ANY
-        };
-
-        errno = 0;
-        if (bind(socketfd, (struct sockaddr *)&local_addr, sizeof(struct sockaddr_in)) < 0) {
-                PRINTF_TO_STDERR(strerror(errno));
-                PRINTF_TO_STDERR(dhcpd_probe_error(DHCPFUNC_ESOCKFD));
+        /* have to bind to netdevice for send packet when no IP address has been assigned */
+        if (setsockopt(socketfd, SOL_SOCKET, SO_BINDTODEVICE, interface, strlen(interface)) < 0) {
+                PRINTF_TO_STDERR(dhcpd_probe_error(DHCPFUNC_EBIND));
                 ret = -1;
                 goto quit_nomalloc;
         }
@@ -62,16 +48,18 @@ int dhcpd_probe(const char *interface)
         ALLOC_DHCP_MSG_BUFFER(msg, DHCP_MSG_SIZE_REPLY_MINIMUM);
         if (!msg) {
                 PRINTF_TO_STDERR(dhcpd_probe_error(DHCPFUNC_EMEMORY));
-                return -1;
+                ret = -1;
+                goto quit_nomalloc;
         }
 
-        ret = makeup_dhcpmsg_discover(msg, HWADDR_802_3_TYPE,
-                                      mac_address, HWADDR_802_3_LEN);
+        ret = makeup_dhcpmsg_discover(msg, ETHER_802_3_TYPE,
+                                      mac_address, ETHER_ADDR_LEN);
         if (ret < 0) {
                 PRINTF_TO_STDERR(dhcpd_probe_error(dhcpfunc_error(ret)));
                 goto quit_malloc;
         }
 
+        /* broadcast address */
         struct sockaddr_in broadcast_addr = {
                 .sin_family = AF_INET,
                 .sin_port = htons(DHCPS_RECV_PORT),
@@ -86,11 +74,40 @@ int dhcpd_probe(const char *interface)
                 goto quit_malloc;
         }
 
-        ret = recv_dhcp_reply_on(socketfd, msg, DHCP_MSG_SIZE_REPLY_MINIMUM);
+        shutdown(socketfd, SHUT_RDWR);
+        socketfd = -1;
+
+        /**
+         * AF_PACKET with SOCK_DGRAM,IP packets subcommited by link-level removed
+         * ethernet frame header.
+         */
+        int sockllfd = socket(AF_PACKET, SOCK_DGRAM, htons(ETH_P_IP));
+        if (sockllfd < 0) {
+                PRINTF_TO_STDERR(dhcpd_probe_error(DHCPFUNC_ESOCKFD));
+                ret = -1;
+                goto quit_malloc;
+        }
+
+        /**
+         * link-level socket address used for bind to network card interface.
+         */
+        struct sockaddr_ll ll_addr = {0};
+        ll_addr.sll_family = AF_PACKET;
+        ll_addr.sll_protocol = htons(ETH_P_IP);
+        ll_addr.sll_ifindex = if_idx();
+
+        if (bind(sockllfd, (struct sockaddr *)&ll_addr, sizeof(struct sockaddr_ll)) < 0) {
+                PRINTF_TO_STDERR(dhcpd_probe_error(DHCPFUNC_EBIND));
+                ret = -1;
+                goto quit_malloc;
+        }
+
+        ret = recv_dhcp_reply_on(sockllfd, msg, DHCP_MSG_SIZE_REPLY_MINIMUM, if_mtu());
         if (ret < 0)
                 PRINTF_TO_STDERR(dhcpd_probe_error(dhcpfunc_error(ret)));
         
 quit_malloc:
+        shutdown(sockllfd, SHUT_RDWR);
         RELEASE_DHCP_MSG_BUFFER(msg);
 
 quit_nomalloc:
